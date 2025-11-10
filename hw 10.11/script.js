000b694b1f62de2d51e98761e25e299d2b907ebc
Anastasios <hadzijanev6@gmail.com>
2025-11-10 16:43:51 +0100

    .

--- hw 10.11/script.js
+++ hw 10.11/script.js
@@ -1,91 +1,91 @@
 
 
-// const Car1 = {
-//     Name: "Audi",
-//     Model: "A3",
-//     year: 2006,
-//     Driven: 230000,
-//     Color: "black",
-//     Price: 5000,
-// };
+const Car1 = {
+    Name: "Audi",
+    Model: "A3",
+    year: 2006,
+    Driven: 230000,
+    Color: "black",
+    Price: 5000,
+};
 
 
 
 
 
-// console.log("name", Car1.Name)
-//     console.log ("Price", Car1.Price)
-// console.log(Car1.Driven, Car1.year, "Driven amount and year");
+console.log("name", Car1.Name)
+    console.log ("Price", Car1.Price)
+console.log(Car1.Driven, Car1.year, "Driven amount and year");
 
 
 
-// console.log("------------");
-// const Car2 = {
-//     Name: "Toyota",
-//     Model: "Yaris",
-//     year: 2006,
-//     Driven: 260000,
-//     Color: "gray",
-//     Price: 4500,
+console.log("------------");
+const Car2 = {
+    Name: "Toyota",
+    Model: "Yaris",
+    year: 2006,
+    Driven: 260000,
+    Color: "gray",
+    Price: 4500,
 
-// }
-// console.log("name", Car2.Name)
-// console.log("Price", Car2.Price);
-// console.log("Driven amount and year", Car2.Driven, Car2.year);
-
-
-
-// console.log("------------");
+}
+console.log("name", Car2.Name)
+console.log("Price", Car2.Price);
+console.log("Driven amount and year", Car2.Driven, Car2.year);
 
 
-// const Car3 = {
-//     Name: "Golf",
-//     Model: 7,
-//     year: 2010,
-//     Driven: 180000,
-//     Color: "dark blue",
-//     Price: 8000,
 
-// }
-// console.log("name", Car3.Name)
-// console.log ("Price", Car3.Price)
-
-// console.log("Driven amount and year",Car3.Driven, Car3.year);
-
-// console.log("------------");
-
-// const Car4 = {
-//     Name: "Mercedes",
-//     Model: "A-class",
-//     year: 2018,
-//     Driven: 180000,
-//     Color: "Gray",
-//     Price: 15000,
-
-// }
-// console.log("name", Car4.Name)
-// console.log ("Price",Car4.Price)
-
-// console.log("Driven amount and year",
-//     Car4.Driven, Car4.year
-// );
+console.log("------------");
 
 
-// console.log("------------");
-// const Car5 = {
-//     Name: "Mercedes Maybach",
-//     Model: "S-class",
-//     year: 2015,
-//     Driven: 100000,
-//     Color: "Dark Gray",
-//     Price: 30000,
+const Car3 = {
+    Name: "Golf",
+    Model: 7,
+    year: 2010,
+    Driven: 180000,
+    Color: "dark blue",
+    Price: 8000,
 
-// }
-// console.log("name", Car5.Name)
-// console.log ("Price", Car5.Price)
-// console.log("Driven amount and year", Car5.Driven, Car5.year);
+}
+console.log("name", Car3.Name)
+console.log ("Price", Car3.Price)
 
-// console.log("------------");
+console.log("Driven amount and year",Car3.Driven, Car3.year);
+
+console.log("------------");
+
+const Car4 = {
+    Name: "Mercedes",
+    Model: "A-class",
+    year: 2018,
+    Driven: 180000,
+    Color: "Gray",
+    Price: 15000,
+
+}
+console.log("name", Car4.Name)
+console.log ("Price",Car4.Price)
+
+console.log("Driven amount and year",
+    Car4.Driven, Car4.year
+);
+
+
+console.log("------------");
+const Car5 = {
+    Name: "Mercedes Maybach",
+    Model: "S-class",
+    year: 2015,
+    Driven: 100000,
+    Color: "Dark Gray",
+    Price: 30000,
+
+}
+console.log("name", Car5.Name)
+console.log ("Price", Car5.Price)
+console.log("Driven amount and year", Car5.Driven, Car5.year);
+
+console.log("------------");
 
 
 
